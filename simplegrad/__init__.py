e66ab2cbde76ac63038471182c1fa05ec7e30d7e
mohamedrxo <mohamedrxo4@gmail.com>
2025-09-28 15:44:05 +0200

    adding dataloader and removing crossentropyloss from init

--- simplegrad/__init__.py
+++ simplegrad/__init__.py
@@ -2,6 +2,7 @@ from .tensor import Tensor   # makes Tensor accessible at top level
 from .losses import MSELoss
 from .optim import SGD
 from .linear import Linear
+from .dataloader  import DataLoader
 
-__all__ = ["Tensor","MSELoss","SGD","Linear"]         # controls what gets imported with *
+__all__ = ["Tensor","MSELoss","SGD","Linear","DataLoader"]         # controls what gets imported with *
 __version__ = "0.1.0"
