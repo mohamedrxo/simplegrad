0cb03e950008cbba9b1b5b0263a3aa9b3a418c97
mohamedrxo <mohamedrxo4@gmail.com>
2025-09-28 15:21:59 +0200

    updating the linear function by adding weights initialization and starting the baies by zeros and param function

--- simplegrad/linear.py
+++ simplegrad/linear.py
@@ -1,17 +1,42 @@
 import numpy as np
 from simplegrad import Tensor
 
-class Linear():
-    def __init__(self,in_dim,out_dim,bias=True):
-        self.w = Tensor(np.random.randn(in_dim,out_dim),requires_grad=True)
-        self.b = Tensor(np.random.randn(out_dim),requires_grad=True) if bias else None
-        self.bias=bias
+# working version
+class Linear:
+    def __init__(self, in_dim, out_dim, bias=True):
+        # Better weight initialization - Xavier/Glorot initialization
+        self.w = Tensor(np.random.randn(in_dim, out_dim) / np.sqrt(in_dim), requires_grad=True)
+        self.b = Tensor(np.zeros(out_dim), requires_grad=True) if bias else None
+        self.bias = bias
         
     def __call__(self, x):
         out = x @ self.w 
         if self.bias:
-            out  =  out + self.b
+            out = out + self.b
         return out
+    
     def parameters(self):
-        return [self.w,self.b]
+        params = [self.w]
+        if self.b is not None:
+            params.append(self.b)
+        return params
+    
+# non working version 
+
+# 1 deviding by sqrt(in_dim) let to better model initialization
+# 2 the bias should ne initualized to zero instead of random
+# 3 [self.w,self.b] can produce [self.w,None]
+
+# class Linear():
+#     def __init__(self,in_dim,out_dim,bias=True):
+#         self.w = Tensor(np.random.randn(in_dim,out_dim),requires_grad=True)
+#         self.b = Tensor(np.random.randn(out_dim),requires_grad=True) if bias else None
+#         self.bias=bias
         
+#     def __call__(self, x):
+#         out = x @ self.w 
+#         if self.bias:
+#             out  =  out + self.b
+#         return out
+#     def parameters(self):
+#         return [self.w,self.b]
